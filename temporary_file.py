7e3da875044937c694e18c827b3d775bea202117
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-08 02:45:20 +0800

    program_10 completed

--- temporary_file.py
+++ temporary_file.py
@@ -1,3 +1,2 @@
 
-# Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
