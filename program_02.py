4a6cb149b9e24214eebdd8406adc8daeb52436c0
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-08 02:24:20 +0800

    completed program_02

--- program_02.py
+++ program_02.py
@@ -1,1 +1,10 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+while True:
+    try:
+        first_number = float(input("enter your first number: "))
+        second_number = float(input("enter your second number: "))
+        if first_number == second_number:
+            print("equal")
+        break
+    except:
+        print("only numbers are allowed")
