0253962667112503679ef972f0ee6a3a0982b0aa
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-08 21:32:06 +0800

    batch 2 program 4 completed

---
+++ program_04.py
@@ -0,0 +1,12 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+while True:
+    try:
+        first_number = float(input("enter your first number: "))
+        second_number = float(input("enter your second number: "))
+        if second_number != 0:
+            print(int(first_number / second_number))
+        else:
+            print("can't divide by zero!")
+        break
+    except:
+        print("only numbers are allowed")
