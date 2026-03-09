715bf4f667d3029091f043528d4a4320dcd16644
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-09 12:51:09 +0800

    batch 5 program 6 completed

---
+++ batch_5/program_06.py
@@ -0,0 +1,3 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+user_name = input("enter your full name in incorrect casing: ")
+print(user_name.swapcase())
