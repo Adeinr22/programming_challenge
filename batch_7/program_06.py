7e002685831af15525aee519192f886c674d774c
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-09 18:43:54 +0800

    batch 7 program 6 completed

---
+++ batch_7/program_06.py
@@ -0,0 +1,23 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies 
+# in function parameter. Create a program that do the same functionality without using rjust() function.
+try:
+    user_input = input("enter whatever you want: ")
+    width_input = int(input("What width do you want it to be (number): "))
+    fill_input = input("what should you fill it with (must be single character): ")
+    if len(user_input) < width_input:
+        print(user_input)
+
+    if len(fill_input) > 1:
+        print("invalid input only a single character is allowed")
+        
+    if fill_input == '':
+        for i in range(width_input - len(user_input)):
+            user_input = ' ' + user_input
+        print(user_input)
+        
+    if len(fill_input) == 1 and fill_input != '':
+        for i in range(width_input - len(user_input)):
+            user_input = fill_input + user_input
+        print(user_input)
+except:
+    print("invalid input")
