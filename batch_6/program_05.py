df57972d65c02d5c501a9399273dd179cc6871aa
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-09 16:38:23 +0800

    batch 6 program 5 completed

---
+++ batch_6/program_05.py
@@ -0,0 +1,13 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same 
+# functionality without using endswith() function.
+user_input = input("enter whatever you want: ")
+end_input = input("what should it end with: ")
+checker = True
+for i in range(len(end_input)):
+    if user_input[(-1 - i)] != end_input[(-1 - i)]:
+        checker = False
+
+if checker:
+    print(user_input + " ends with " + end_input)
+else:
+    print(user_input + " does not end with " + end_input)
