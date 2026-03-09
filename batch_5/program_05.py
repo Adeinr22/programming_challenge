e2356ae27d402ec989384573844c347f6213551b
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-09 12:47:09 +0800

    batch 5 program 5 completed

---
+++ batch_5/program_05.py
@@ -0,0 +1,3 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+user_name = input("enter your full name in incorrect casing: ")
+print(user_name.capitalize())
