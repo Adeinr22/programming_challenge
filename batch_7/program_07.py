8acc8a6bc72e2442fd54cb711e0fd1d68c4bc1d3
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-09 19:05:51 +0800

    batch 7 program 7 completed

---
+++ batch_7/program_07.py
@@ -0,0 +1,18 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies 
+# in function parameter. Create a program that do the same functionality without using zfill() function.
+try:
+    user_input = input("Enter whatever you want: ")
+    width_input = int(input("What width do you want it to be (number): "))
+
+    if width_input < 0:
+        print(user_input)
+
+    elif len(user_input) < width_input:
+        zeros_needed = width_input - len(user_input)
+        zeros = '0' * zeros_needed 
+        print(zeros + user_input)
+    else:
+        print(user_input)
+
+except:
+    print("Invalid input")
