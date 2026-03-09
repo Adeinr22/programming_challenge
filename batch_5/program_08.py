ba6cfaa0a510d9b0765a4944c7e692301b58334a
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-09 13:01:17 +0800

    batch 5 program 8

---
+++ batch_5/program_08.py
@@ -0,0 +1,4 @@
+# Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+user_name = input("enter your full name: ")
+print(len(user_name))
+# note: space is also a character
