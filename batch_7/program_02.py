5db5f0a4b9bf192bae8af210ba6b0ed2ba912633
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-17 02:39:56 +0800

    batch 7 program 2 improved

--- batch_7/program_02.py
+++ batch_7/program_02.py
@@ -1,12 +1,11 @@
 # Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program 
 # that do the same functionality without using removesuffix() function.
-user_input = input("enter whatever you want: ")
-user_remove = input("enter what you want to remove at the end: ")
-final_output = []
-for char in user_input:
-    final_output.append(char)
-for i in range(len(user_remove), -1, -1):
-    if user_remove[i - 1] == final_output[-1]:
-        final_output.pop()
-for i in final_output:
-    print(i, end="")
+user_input = input("Enter whatever you want: ")
+user_remove = input("Enter what you want to remove at the end: ")
+
+if len(user_remove) <= len(user_input) and user_input[-len(user_remove):] == user_remove:
+    result = user_input[:-len(user_remove)]
+else:
+    result = user_input
+
+print(result)
