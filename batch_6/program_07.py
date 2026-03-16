7ffbdf3beab8120b639fbd0ae1882c55921260c3
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-17 02:23:01 +0800

    batch 6 program 7 improved

--- batch_6/program_07.py
+++ batch_6/program_07.py
@@ -1,25 +1,24 @@
 # Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a 
 # program that do the same functionality without using center() function.
 try:
-    user_input = input("enter whatever you want: ")
+    user_input = input("Enter whatever you want: ")
     width_input = int(input("What width do you want it to be (number): "))
-    fill_input = input("what should you fill it with (must be single character): ")
-    if len(user_input) < width_input:
-        print(user_input)
+    fill_input = input("What should you fill it with (must be a single character): ")
 
-    if len(fill_input) > 1:
-        print("invalid input only a single character is allowed")
-        
-    if fill_input == '':
-        for i in range(int((width_input - len(user_input)) / 2)):
-            user_input = user_input + " "
-            user_input = " "+ user_input 
-        print(user_input)
-        
-    if len(fill_input) == 1 and fill_input != '':
-        for i in range(int((width_input - len(user_input)) / 2)):
-            user_input = user_input + fill_input
-            user_input = fill_input + user_input 
-        print(user_input)
-except:
-    print("invalid input")
+    if len(fill_input) != 1:
+        print("Error: fill character must be exactly one character.")
+        import sys
+        sys.exit(1)
+
+    if width_input <= len(user_input):
+        result = user_input
+    else:
+        total_padding = width_input - len(user_input)
+        left_padding = total_padding // 2          
+        right_padding = total_padding - left_padding 
+        result = fill_input * left_padding + user_input + fill_input * right_padding
+
+    print(result)
+
+except ValueError:
+    print("Invalid input: width must be an integer.")
