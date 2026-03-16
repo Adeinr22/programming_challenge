6bba67c1d4ebf59037de39320a622de6b3f9f56b
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-17 02:35:40 +0800

    batch 7 program 1 improved

--- batch_7/program_01.py
+++ batch_7/program_01.py
@@ -1,10 +1,10 @@
 # Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without 
 # using rstrip() function.
 user_input = input("enter something with spaces at the end: ")
-removed_spaces = []
-for char in user_input:
-    removed_spaces.append(char)
-while removed_spaces[-1] == ' ':
-    removed_spaces.pop()
-for i in removed_spaces:
-    print(i, end='')
+removed_spaces = 0
+for i in range(-1, (-len(user_input)-1), -1):
+    if user_input[i] == " ":
+        removed_spaces += 1
+    else:
+        break
+print(user_input[:len(user_input) - removed_spaces])
