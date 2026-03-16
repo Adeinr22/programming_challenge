5948e96fd3300a7307c1dd02aaf096190fccad5b
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-17 02:49:50 +0800

    batch 7 program 5 improved

--- batch_7/program_05.py
+++ batch_7/program_05.py
@@ -3,11 +3,14 @@
 user_input = input("enter whatever you want: ")
 start_input = input("what should it start with: ")
 checker = True
-for i in range(len(start_input)):
-    if user_input[i] != start_input[i]:
-        checker = False
+if len(start_input) > len(user_input):
+    checker = False
+else:
+    for i in range(len(start_input)):
+        if user_input[i] != start_input[i]:
+            checker = False
 
 if checker:
     print(user_input + " starts with " + start_input)
 else:
-    print(user_input + " does not start with " + start_input)
+    print(user_input + " does not start with " + start_input)   
