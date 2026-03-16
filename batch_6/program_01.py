54245d85ae949fe4f2aac9d689ee64eed7538639
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-17 01:07:15 +0800

    improved batch 6 program 1

--- batch_6/program_01.py
+++ batch_6/program_01.py
@@ -1,10 +1,10 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality 
 # without using lstrip() function.
 user_input = input("enter something with spaces at the beginning: ")
-removed_spaces = ""
+removed_spaces = 0
 for char in user_input:
-    if char != " " and removed_spaces == "":
-        removed_spaces += char
-    elif removed_spaces != "":
-        removed_spaces += char
-print(removed_spaces)
+    if char == " ":
+        removed_spaces += 1
+    else:
+        break
+print(user_input[removed_spaces:])
