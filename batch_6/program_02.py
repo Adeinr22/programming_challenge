8a805054aa5c1ba2964948169c65d6a6fa5aa0b7
Adeinr22 <andrei05.jules22@gmail.com>
2026-03-17 01:20:34 +0800

    batch 6 program 2 improved

--- batch_6/program_02.py
+++ batch_6/program_02.py
@@ -2,11 +2,7 @@
 # Create a program that do the same functionality without using removeprefix() function.
 user_input = input("enter whatever you want: ")
 user_remove = input("enter what you want to remove at the beginning: ")
-final_output = []
-for char in user_input:
-    final_output.append(char)
-for char in user_remove:
-    if char == final_output[0]:
-        final_output.pop(0)
-for i in final_output:
-    print(i, end="")
+if user_remove == user_input[:len(user_remove)]:
+    print(user_input[len(user_remove):])
+else:
+    print(user_input)
